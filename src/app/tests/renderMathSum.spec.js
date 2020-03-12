597a979e746024d1988a615f4088b47869ce224c
Raphael Brand <info@raphaelbrand.de>
2020-03-13 00:18:43 +0100

    add click handler to calculate the current task - amend css file ... - add initial value on click - filter matrix values

--- src/app/tests/renderMathSum.spec.js
+++ src/app/tests/renderMathSum.spec.js
@@ -7,12 +7,10 @@ import MathSumRenderer from './../actions/renderMathSum'
 describe('math sum field generate', () => {
     let generator = new PlayfieldGenerator();
     let renderer = new MathSumRenderer({matrix: generator.restart()});
-    
     it('calls the main function', () => {
         generator.init()
         console.log(renderer)
     });
-    
     it('calls the update function', () => {
         console.log(renderer.init());
         console.log(renderer.update());
