73b08423b634169d4fe9a0b2b60b28351b5aef40
Raphael Brand <info@raphaelbrand.de>
2020-04-21 21:05:53 +0200

    add document title

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+document.title = '1-Minute-Math-Test';
 ReactDOM.render(
     <App />,
     document.querySelector('#app')
