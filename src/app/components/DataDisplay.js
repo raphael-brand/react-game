7d785d537a735ea318efa83dc8e66e081f8ec140
Raphael Brand <info@raphaelbrand.de>
2020-06-12 13:09:37 +0200

    try fix image loading issue and prevent the countdown to start earlier

--- src/app/components/DataDisplay.js
+++ src/app/components/DataDisplay.js
@@ -1,16 +1,24 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component, useEffect, useRef, createRef } from 'react';
 import PropTypes from 'prop-types';
 import Countdown from './Countdown';
 import { gearSymbol } from '../assets';
 
 const DataDisplay = (props) => {
 
-    
+    let loadDelay = true;
+
+    let gear = createRef();
+    useEffect(cb => {
+        gear.current.addEventListener('load', () => {
+            loadDelay = false;
+        });
+    })
+
     return (
     <div className="data-display-container">
-        <div className="current-task">{ props.displayValue }</div>
-        <Countdown startValue={props.countdown} onComplete={props.onCountdownFinish} gameIsPaused={props.gameIsPaused}/>
-        <div id="setup">{gearSymbol(props.openMainMenu)}</div>
+        {<div className="current-task">{ props.displayValue ? props.displayValue : '' }</div>}
+        <Countdown startValue={props.countdown} onComplete={props.onCountdownFinish} gameIsPaused={() => loadDelay ? !loadDelay : props.gameIsPaused()}/>
+        <div id="setup" className="animate-in"><img src="gear_2699.png" width="47" height="47" onClick={props.openMainMenu} ref={gear} /></div>
     </div>
     );
 }
