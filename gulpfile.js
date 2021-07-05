827d93da132f0592b233f0421f44ffed2f900ade
Raphael Brand <info@raphaelbrand.de>
2021-07-05 13:20:46 +0000

    updating postcss and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp')
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var concatCSS = require('gulp-concat-css');
 gulp.task('sass', function () {
     const autoprefixer = require('autoprefixer')
